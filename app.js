25d1296444eec7e3bf2b83a81c1583253781234a
Edmond Bitay <edmond.bitay@gmail.com>
2018-01-29 13:30:51 +0100

    added multer middleware to manage upload of file and display attributes such as file size

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var fs = require("fs");
 var app = express();
+var multer = require("multer");
+var upload = multer({ dest: __dirname + '/uploads/' });
 
 
 app.get('/filemeta', function(req, res){
 	var index = fs.createReadStream(__dirname + '/assets/index.html', 'utf8');
 	res.writeHead(200, {"Content-Type":"text/html"});
 	index.pipe(res);
+});
+
+app.post('/filemeta', upload.single('file'), function(req, res, next){
+	console.log(JSON.stringify(req.file));
+	res.writeHead(200, {"Content-Type":"text/json"});
+	res.end(JSON.stringify({name: req.file.originalname, size: req.file.size + " bytes"}));
 });
 
 app.use("/assets", express.static('assets'));
